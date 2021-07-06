9f0d5b6caa018b2853d315638096bb3ba7abbf5a
ladyruo <3126085779@qq.com>
2021-07-06 11:07:31 +0800

    弹窗

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -5,30 +5,72 @@ import Spacer from "../Spacer";
 import CardButton from "../CardButton";
 
 const Select: React.FC = ({}) => (
-  <CardButton className="wing-blank-lg" size="lg">
-  
-
-    <div className="flex-jc-center">
+  <div className="position-relative">
+    <CardButton className="wing-blank-lg " size="lg">
       <div className="flex-jc-center">
-        <img
-          src={require("../../assets/img/ETH_icon.png")}
-          width="35"
-          height="35"
-          className="margin-right-10"
-        />
-        ETH
-        <img
-          src={require("../../assets/img/arrow_bottom_icon.png")}
-          width="8"
-          height="5"
-          className="margin-left-10"
-        />
+        <div className="flex-jc-center">
+          <img
+            src={require("../../assets/img/ETH_icon.png")}
+            width="35"
+            height="35"
+            className="margin-right-10"
+          />
+          ETH
+          <img
+            src={require("../../assets/img/arrow_bottom_icon.png")}
+            width="8"
+            height="5"
+            className="margin-left-10"
+          />
+        </div>
+        <div>0.000001</div>
       </div>
-      <div>0.000001</div>
-    </div>
-
-   
-  </CardButton>
+      <StyledSelectBox className="form-select-content  cursor-pointer">
+        <StyledSelectItem className="flex-jc-center  wing-blank-lg ">
+          <div className="flex-jc-start">
+            <img
+              src={require("../../assets/img/ETH_icon.png")}
+              width="35"
+              height="35"
+              className="margin-right-10"
+            />
+            ETH
+          </div>
+          <div>0.000001</div>
+        </StyledSelectItem>
+        <StyledSelectItem className="flex-jc-center  wing-blank-lg active">
+          <div className="flex-jc-start">
+            <img
+              src={require("../../assets/img/ETH_icon.png")}
+              width="35"
+              height="35"
+              className="margin-right-10"
+            />
+            ETH
+          </div>
+          <div>0.000001</div>
+        </StyledSelectItem>
+      </StyledSelectBox>
+    </CardButton>
+  </div>
 );
+
+const StyledSelectBox = styled.div`
+  position: absolute;
+  width: 100%;
+  left: 0;
+  border-radius: 40px;
+  top: 65px;
+  z-index: 99;
+  background: #fff;
+  box-shadow: 0px 0px 10px ${(props) => props.theme.color.grey[200]};
+`;
+const StyledSelectItem = styled.div`
+  height: 50px;
+  border-radius: 40px;
+  &.active {
+    background-color: ${(props) => props.theme.color.grey[500]};
+  }
+`;
 
 export default Select;
