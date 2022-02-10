a64ad9205bc0b99d82bcd0e1b61c62732851e144
tunogya <tunogya@qq.com>
2022-02-10 12:20:20 +0800

    fixed style

--- src/views/Mine/components/List.tsx
+++ src/views/Mine/components/List.tsx
@@ -1,8 +1,8 @@
 //@ts-nocheck
-import React, { useMemo } from "react";
+import React, {useMemo} from "react";
 import styled from "styled-components";
-import { useHistory } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import {useHistory} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 import Spacer from "../../../components/Spacer";
 import Card from "../../../components/Card";
 import Button from "../../../components/Button";
@@ -12,11 +12,12 @@ import Value from "../../../components/Value";
 import useIsMobile from "../../../hooks/useIsMobile";
 import useMineInfo from "../../../hooks/mine/useMineInfo";
 import useItank from "../../../hooks/itank/useItank";
-import { getNumberToMax } from "../../../utils/formatBalance";
-const Item: React.FC = ({ item }) => {
+import {getNumberToMax} from "../../../utils/formatBalance";
+
+const Item: React.FC = ({item}) => {
   const isMobile = useIsMobile();
   const history = useHistory();
-  const { t } = useTranslation();
+  const {t} = useTranslation();
   const itank = useItank(item.depositContract);
 
   const mineInfo = useMineInfo(item, itank);
@@ -25,7 +26,7 @@ const Item: React.FC = ({ item }) => {
     mineInfo.staked,
   ]);
   const status = useMemo(() => {
-    const rewardRate =  mineInfo.info.rewardRate;
+    const rewardRate = mineInfo.info.rewardRate;
     return {
       color: rewardRate > 0 ? "#0CC69D" : "#878787",
       text: rewardRate > 0 ? "jxz" : "yzt",
@@ -35,14 +36,14 @@ const Item: React.FC = ({ item }) => {
     <>
       <StyledWrapBox className={`wing-blank-lg bg-white ${isMobile ? "" : "with-100-24"} `}>
         <StyledTag color={status.color}>{t(status.text)}</StyledTag>
-        <Spacer size="mmd" />
+        <Spacer/>
         <div className="flex-row-center-center">
-          <TokenSymbol symbol={item.icon1} size={40} />
-          <TokenSymbol symbol={item.icon2} size={40} isRight={true} />
+          <TokenSymbol symbol={item.icon1} size={40}/>
+          <TokenSymbol symbol={item.icon2} size={40} isRight={true}/>
         </div>
-        <Spacer size="sm" />
+        <Spacer size="sm"/>
         <div className="font-size-16 text-center">{item.name}</div>
-        <Spacer size="ssm" />
+        <Spacer size="sm"/>
         {/* {{lpToken}} Earn {{token}} */}
         <div className="color-grey  text-center">
           {t("cunzhuan", {
@@ -50,24 +51,24 @@ const Item: React.FC = ({ item }) => {
             token: item.earnTokenName,
           })}
         </div>
-        <Spacer size="mmd" />
+        <Spacer/>
 
-        <Label label="TVL" value={<Value value={mineInfo.tvl} prefix="$" />} />
-        <Spacer size="mmd" />
-        <Label label="APY" value={<Value value={mineInfo.apy} suffix="%" />} />
+        <Label label="TVL" value={<Value value={mineInfo.tvl} prefix="$"/>}/>
+        <Spacer/>
+        <Label label="APY" value={<Value value={mineInfo.apy} suffix="%"/>}/>
 
-        <Spacer size="mmd" />
+        <Spacer/>
         <Label
           label={`${t("wdzy")} (${item.depositTokenName})`}
-          value={<Value value={staked} />}
+          value={<Value value={staked}/>}
         />
 
-        <Spacer size="mmd" />
+        <Spacer/>
         <Label
           label={`${t("dlqsy")} (${item.earnTokenName})`}
-          value={<Value value={mineInfo.earned} />}
+          value={<Value value={mineInfo.earned}/>}
         />
-        <Spacer />
+        <Spacer/>
         <Button
           text={t("xuanze")}
           variant="secondary"
@@ -75,18 +76,18 @@ const Item: React.FC = ({ item }) => {
             history.push(`/mine/pool/${item.depositContract}`);
           }}
         />
-        <Spacer size="mmd" />
+        <Spacer/>
       </StyledWrapBox>
     </>
   );
 };
-const List: React.FC = ({ mines }) => {
+const List: React.FC = ({mines}) => {
   const isMobile = useIsMobile();
   return (
     <>
       <div className={`width-100 ${isMobile ? "" : "flex-jc-center"} `}>
         {mines.map((item, i) => {
-          return <Item item={item} key={item.name} />;
+          return <Item item={item} key={item.name}/>;
         })}
       </div>
       <Spacer/>
@@ -105,7 +106,7 @@ const StyledTag = styled.div`
   position: absolute;
   left: 0;
   top: 20px;
-  background: ${({ color }) => color};
+  background: ${({color}) => color};
   border-radius: 0px 20px 20px 0px;
   height: 30px;
   display: flex;
